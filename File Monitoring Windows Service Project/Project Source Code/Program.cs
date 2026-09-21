fbf49a0177cbc58457ea7fa1bdca2f24073b060b
jafr jaber <jafrsyd543@gmail.com>
2026-09-21 16:09:13 +0300

    Add files via upload

---
+++ File Monitoring Windows Service Project/Project Source Code/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_Monitoring_Windows_Service
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+
+
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                     new File_Monitoring()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+
+        //Test Mode
+        //static void Main()
+        //{
+        //    if (Environment.UserInteractive)
+        //    {
+        //        Console.WriteLine("Running in Console Mode");
+        //        File_Monitoring file_Monitoring = new File_Monitoring();
+        //        file_Monitoring.TestOnConsole();
+        //    }
+        //    else
+        //    {
+        //        ServiceBase[] ServicesToRun;
+        //        ServicesToRun = new ServiceBase[]
+        //        {
+        //             new File_Monitoring()
+        //        };
+        //         ServiceBase.Run(ServicesToRun);
+        //    }
+
+        //}
+    }
+}
---
+++ File Monitoring Windows Service Project/Project Source Code/ProjectInstaller.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.ServiceProcess;
+
+namespace File_Monitoring_Windows_Service
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        ServiceProcessInstaller processInstaller;
+        ServiceInstaller serviceInstaller;
+
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+
+            processInstaller = new ServiceProcessInstaller
+            {
+                Account = ServiceAccount.LocalSystem
+            };
+
+            serviceInstaller = new ServiceInstaller
+            {
+                ServiceName = "FileMonitoringService",
+                DisplayName = "FileMonitoringService",
+                Description = @"A Windows Service that Watch and Move Files From specific
+                                Folder to another Folder With Rename File to GUID Name",
+                StartType = ServiceStartMode.Automatic
+            };
+
+            Installers.Add(processInstaller);
+            Installers.Add(serviceInstaller);
+        }
+    }
+}
