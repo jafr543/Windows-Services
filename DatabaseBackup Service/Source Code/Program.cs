2cc36805139431e0522b0f3f4dd5408ab35d7e8a
jafr jaber <jafrsyd543@gmail.com>
2026-09-22 14:31:30 +0300

    Add files via upload

---
+++ DatabaseBackup Service/Source Code/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseBackup_Service
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new DatabaseBackup()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+
+        //Test Mode
+        //static void Main()
+        //{
+        //    if (Environment.UserInteractive)
+        //    {
+        //        Console.WriteLine("Running in Console Mode");
+        //        DatabaseBackup databaseBackup = new DatabaseBackup();
+        //        databaseBackup.TestOnConsole();
+        //    }
+        //    else
+        //    {
+        //        ServiceBase[] ServicesToRun;
+        //        ServicesToRun = new ServiceBase[]
+        //        {
+        //             new DatabaseBackup()
+        //        };
+        //        ServiceBase.Run(ServicesToRun);
+        //    }
+
+        //}
+    }
+}
---
+++ DatabaseBackup Service/Source Code/ProjectInstaller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace DatabaseBackup_Service
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        ServiceProcessInstaller processInstaller;
+        ServiceInstaller serviceInstaller;
+
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+
+            processInstaller = new ServiceProcessInstaller
+            {
+                Account = ServiceAccount.LocalSystem
+            };
+
+            serviceInstaller = new ServiceInstaller
+            {
+                ServiceName = "DataBaseBackup",
+                DisplayName = "DataBase Backup Service",
+                Description ="A Windows Service that automatically backs up a SQL Server database at a configurable interval.",
+                StartType = ServiceStartMode.Automatic,
+                // Define dependencies
+                ServicesDependedOn = new string[]
+                {
+                    "MSSQLSERVER", // SQL Server default instance (adjust for named instances)
+                    "RpcSs",       // Remote Procedure Call
+                    "EventLog"     // Windows Event Log
+                }
+            };
+
+            Installers.Add(processInstaller);
+            Installers.Add(serviceInstaller);
+        }
+    }
+}
